c77801519dcfd06e9e0b8581ed5b782df1245307
aguilam <pexfex@mail.ru>
2023-11-17 20:57:12 +0400

    add calculation, but have very bad bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,27 @@
 import { useState } from 'react'
 
 export default function Home() {
-  const [ answer , setAnswer ] = useState('20')
+  const [ answer , setAnswer ] = useState('5')
   const [ userAnswer , setUserAnswer ] = useState('0')
-  const [ question , setQuestion ] = useState('4*5')
-  const [ questions , setQuestions ] = useState(['','','','',''])
+  const [ question , setQuestion ] = useState('5*1')
+  const [ questions , setQuestions ] = useState(['5*2','5*3','5*4','5*5','5*6'])
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserAnswer(e.target.value);
+    console.log(e.target.value)
   };
 
   const checkQuestion = () => {
     const calculateAnswer = new Function('return ' + question)();
-    const correctAnswer = calculateAnswer.toString();
-    setAnswer(correctAnswer)
+    console.log(calculateAnswer)
+    setAnswer(calculateAnswer.toString())
+    console.log(answer)
+    
     if (userAnswer === answer) {
       alert('Правильно!')
+      setQuestion(questions[0]);
+      console.log(question)
+      setQuestions((prevQuestions) => prevQuestions.slice(1));
+      console.log(questions)
     } else {
       alert('Неправильно!')
     }
