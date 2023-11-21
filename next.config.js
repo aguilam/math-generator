6d53297deb1778ba4eba3110bb45482328fccc83
aguilam <pexfex@mail.ru>
2023-11-21 13:35:58 +0400

    indexing test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+  
+}
+module.exports = {
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'all',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
+};
 module.exports = nextConfig
